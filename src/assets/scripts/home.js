eb167bccf551a431d341f93e5e56eb50ab0366c5
rilton bispo <riltondev@gmail.com>
2023-10-31 11:14:07 -0300

    :recycle: Home main

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -31,32 +31,32 @@ const games = [
   }
 ];
 
-const gameListContainers = document.querySelectorAll('.game-list');
+const gameListContainers = document.querySelectorAll('.game__list');
 
 gameListContainers.forEach((container) => {
   games.forEach((game) => {
     const gameItem = document.createElement('div');
-    gameItem.classList.add('sec-main__container-game');
+    gameItem.classList.add('container__game');
 
     gameItem.innerHTML = `
-      <img src="${game.img}" alt="${game.title}" class="game-image">
-      <div>
-        <div>
+      <img src="${game.img}" alt="${game.title}" class="game__image">
+      <div class="game__info">
+        <div class="game__rating">
           <img src="assets/img/star-fill.svg" alt="Estrela preenchida">
           <img src="assets/img/star-fill.svg" alt="Estrela preenchida">
           <img src="assets/img/star-fill.svg" alt "Estrela preenchida">
           <img src="assets/img/star.svg" alt="Estrela vazia">
           <img src="assets/img/star.svg" alt="Estrela vazia">
         </div>
-        <div class="info-game">
-          <h3>${game.title}</h3>
+        <div class="game__details">
+          <h3 class="game__title">${game.title}</h3>
           <button class="btn btn__secondary">Editar</button>
         </div>
-        <div class="container-tags">
-          <div class="container-tags__tags-category">
+        <div class="game__tags">
+          <div class="game__tags-category">
             ${game.categorys.map((category) => `<div class="tag__game"><p>${category}</p></div>`).join('')}
           </div>
-          <div class="container-tags__tags-platform">
+          <div class="game__tags-platform">
             ${game.plataform.map((platform) => `<div class="tag__plataform"><p>${platform}</p></div>`).join('')}
           </div>
         </div>
