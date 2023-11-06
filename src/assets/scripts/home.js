7ff246b2521a99aa8cb22671dbe6915ca4893d4c
rilton bispo <riltondev@gmail.com>
2023-11-06 16:19:03 -0300

    :sparkles: Crud platform

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -1,12 +1,11 @@
 const categories = ['jogado', 'jogando', 'zerado', 'nao-recomendo'];
-const plataforms = ['Steam', 'Epic', 'PSN', 'Xbox', 'PlayStore'];
+let platformsArray = ['Steam', 'Epic', 'PSN', 'Xbox', 'PlayStore'];
 const checkCategories = document.getElementById("categories-select");
-const checkPlatforms = document.getElementById("platforms-select");
 const checkGames = document.getElementById("games-select");
-const platformList = document.getElementById("platform-list");
 const gameList = document.getElementById('game-list');
+const platformInput = document.getElementById('platform-name')
 
-const games = [
+let games = [
   {
     id: 1,
     title: 'Stardew Valley',
@@ -34,39 +33,42 @@ const games = [
 ];
 
 // ADICIONA OS JOGOS NA HOME
-games.map(game => {
-  const gameItem = document.createElement('div');
-  gameItem.classList.add('container__game')
+const loadGames = (games) => {
+  games.map(game => {
+    const gameItem = document.createElement('div');
+    gameItem.classList.add('container__game')
 
-  gameItem.innerHTML = `
-    <img src="${game.img}" alt="${game.title}" class="game__image">
-    <div class="game__info">
-      <div class="game__rating">
-        <img src="assets/img/star-fill.svg" alt="Estrela preenchida">
-        <img src="assets/img/star-fill.svg" alt="Estrela preenchida">
-        <img src="assets/img/star-fill.svg" alt "Estrela preenchida">
-        <img src="assets/img/star.svg" alt="Estrela vazia">
-        <img src="assets/img/star.svg" alt="Estrela vazia">
-      </div>
-      <div class="game__details">
-        <h3 class="game__title">${game.title}</h3>
-        <button type="button" class="btn btn__secondary" data-toggle="modal" data-target="#editGame-modal">
-          Editar
-        </button>
-      </div>
-      <div class="game__tags">
-        <div class="game__tags-category">
-          ${game.categorys.map((category) => `<div class="tag__game"><p>${category}</p></div>`).join('')}
+    gameItem.innerHTML = `
+      <img src="${game.img}" alt="${game.title}" class="game__image">
+      <div class="game__info">
+        <div class="game__rating">
+          <img src="assets/img/star-fill.svg" alt="Estrela preenchida">
+          <img src="assets/img/star-fill.svg" alt="Estrela preenchida">
+          <img src="assets/img/star-fill.svg" alt "Estrela preenchida">
+          <img src="assets/img/star.svg" alt="Estrela vazia">
+          <img src="assets/img/star.svg" alt="Estrela vazia">
         </div>
-        <div class="game__tags-platform">
-          ${game.plataform.map((platform) => `<div class="tag__plataform"><p>${platform}</p></div>`).join('')}
+        <div class="game__details">
+          <h3 class="game__title">${game.title}</h3>
+          <button type="button" class="btn btn__secondary" data-toggle="modal" data-target="#editGame-modal">
+            Editar
+          </button>
+        </div>
+        <div class="game__tags">
+          <div class="game__tags-category">
+            ${game.categorys.map((category) => `<div class="tag__game"><p>${category}</p></div>`).join('')}
+          </div>
+          <div class="game__tags-platform">
+            ${game.plataform.map((platform) => `<div class="tag__plataform"><p>${platform}</p></div>`).join('')}
+          </div>
         </div>
       </div>
-    </div>
-  `;
+    `;
 
-  gameList.appendChild(gameItem)
-})
+    gameList.appendChild(gameItem)
+  })
+}
+loadGames(games)
 
 // ADICIONA OS INPUTS DE CATEGORIA NO MODAL-GAME
 categories.map(category => {
@@ -82,39 +84,54 @@ categories.map(category => {
   checkCategories.appendChild(label);
 });
 
-// ADICIONA OS INPUTS DE PLATAFORMAS NO MODAL-GAME
-plataforms.map(platform => {
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.id = platform;
+// GERENCIA AS PLATAFORMAS
+const loadPlatform = (platformsArray) => {
+  const platformList = document.getElementById("platform-list");
+  const checkPlatforms = document.getElementById("platforms-select");
 
-  const label = document.createElement('label');
-  label.htmlFor = platform;
-  label.appendChild(checkbox);
-  label.appendChild(document.createTextNode(platform));
+  platformList.innerHTML = '';
+  platformsArray.map(platform => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('platform-item');
 
-  checkPlatforms.appendChild(label);
-});
+    listItem.innerHTML = `
+      <div class="platform-input__container">
+        <input type="text" id=${platform} name=${platform} value=${platform} class="platform-input"></input>
+        <img src="assets/img/pen-icon.svg" class="platform-icon"/>
+      </div>
+      <img src="assets/img/icon-trash.svg" class="platform-image" onclick="removePlatform('${platform}')"/>
+    `;
 
-// ADICIONA OS INPUTS NO MODAL-PLATFORM
-plataforms.map(platform => {
-  const listItem = document.createElement('li');
-  listItem.classList.add('platform-item');
+    platformList.appendChild(listItem);
+  });
 
-  listItem.innerHTML = `
-    <div class="platform-input__container">
-      <input type="text" id=${platform} name=${platform} value=${platform} class="platform-input"></input>
-      <img src="assets/img/pen-icon.svg" class="platform-icon"/>
-    </div>
-    <img src="assets/img/icon-trash.svg" class="platform-image" onclick="removePlatform(this)"/>
-  `;
+  checkPlatforms.innerHTML = ''
+  platformsArray.map(platform => {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = platform;
 
-  platformList.appendChild(listItem);
-});
+    const label = document.createElement('label');
+    label.htmlFor = platform;
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(platform));
 
-function removePlatform(element) {
-  const platformItem = element.parentElement;
-  platformList.removeChild(platformItem);
+    checkPlatforms.appendChild(label);
+  });
+}
+loadPlatform(platformsArray)
+
+// CRIA A PLATAFORMA
+const createPlatform = () => {
+  platformsArray.push(platformInput.value.trim());
+  loadPlatform(platformsArray);
+}
+
+// DELETA A PLATAFORMA
+function removePlatform(platformDel) {
+  const newArray = platformsArray.filter(platform => platform !== platformDel);
+  platformsArray = newArray;
+  loadPlatform(platformsArray);
 }
 
 // MOSTRAR DROPDOWN CATEGORIA
